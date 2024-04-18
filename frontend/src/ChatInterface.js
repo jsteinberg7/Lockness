647a132581e93fa57c0f7caa49f1ff2991a5f440
yuvansundrani <yuvan.sundrani@gmail.com>
2024-04-18 16:28:33 -0400

    chat interface UI cleanup

--- frontend/src/ChatInterface.js
+++ frontend/src/ChatInterface.js
@@ -1,142 +1,195 @@
 import React, { useState, useEffect } from "react";
-import { Box, VStack, HStack, Button, Text, Textarea, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  VStack,
+  HStack,
+  Button,
+  Text,
+  Textarea,
+  Spinner,
+  Flex,
+  InputGroup,
+  InputRightElement,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import io from "socket.io-client";
 import { ArrowUpIcon } from "@chakra-ui/icons";
 import logo from "./assets/locknessLogo.png";
+import { TriangleUpIcon } from "@chakra-ui/icons"; // Or any other appropriate icon
 
 // get active domain
 const domain = window.location.hostname;
 
 const socket = domain.includes("localhost")
-    ? io("http://localhost:5001")
-    : io("https://lockness-420607.uc.r.appspot.com");
+  ? io("http://localhost:5001")
+  : io("https://lockness-420607.uc.r.appspot.com");
 
 const ChatInterface = () => {
-    const [messages, setMessages] = useState([
-        {
-            text: "Hello! I am Lockness. How can I help you today?",
-            sender: "bot",
-        },
-    ]);
-    const [inputMessage, setInputMessage] = useState();
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+  const [messages, setMessages] = useState([
+    // {
+    //   text: "Hello! I am Lockness. How can I help you today?",
+    //   sender: "bot",
+    // },
+  ]);
+  const [inputMessage, setInputMessage] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        let currentMessage = { text: "", sender: "bot" };
+  useEffect(() => {
+    let currentMessage = { text: "", sender: "bot" };
 
-        socket.on("new_message", (message) => {
-            setLoading(false);
-            if (!message.final && message.text) {
-                currentMessage.text += message.text;
-                setMessages((prevMessages) => {
-                    // Update the last bot message if it exists, otherwise add a new one
-                    if (
-                        prevMessages.length &&
-                        prevMessages[prevMessages.length - 1].sender === "bot"
-                    ) {
-                        return [...prevMessages.slice(0, -1), currentMessage];
-                    } else {
-                        return [...prevMessages, currentMessage];
-                    }
-                });
-            } else if (message.final && currentMessage.text) {
-                // Push the complete message only if there's text in the current message
-                // setMessages(prevMessages => [...prevMessages, currentMessage]);
-                currentMessage = { text: "", sender: "bot" }; // Reset for the next message
-            }
+    socket.on("new_message", (message) => {
+      setLoading(false);
+      if (!message.final && message.text) {
+        currentMessage.text += message.text;
+        setMessages((prevMessages) => {
+          // Update the last bot message if it exists, otherwise add a new one
+          if (
+            prevMessages.length &&
+            prevMessages[prevMessages.length - 1].sender === "bot"
+          ) {
+            return [...prevMessages.slice(0, -1), currentMessage];
+          } else {
+            return [...prevMessages, currentMessage];
+          }
         });
+      } else if (message.final && currentMessage.text) {
+        // Push the complete message only if there's text in the current message
+        // setMessages(prevMessages => [...prevMessages, currentMessage]);
+        currentMessage = { text: "", sender: "bot" }; // Reset for the next message
+      }
+    });
 
-        socket.on("connect_error", (err) => {
-            setError("WebSocket connection failed.");
-        });
+    socket.on("connect_error", (err) => {
+      setError("WebSocket connection failed.");
+    });
 
-        return () => {
-            socket.off("new_message");
-            socket.off("connect_error");
-        };
-    }, []);
-
-    const handleSendMessage = () => {
-        if (!inputMessage.trim()) {
-            setError("Please enter a message.");
-            return;
-        }
-        socket.emit("send_prompt", { prompt: inputMessage });
-        // Do not clear the inputMessage here if you want to retain the input until it's manually cleared
-        setMessages((prevMessages) => [
-            ...prevMessages,
-            { text: inputMessage, sender: "user" },
-        ]);
-        setInputMessage("");
-        setLoading(true);
+    return () => {
+      socket.off("new_message");
+      socket.off("connect_error");
     };
+  }, []);
 
-    return (
-        <Box
-            bg="#3E4B5C"
+  const handleSendMessage = () => {
+    if (!inputMessage.trim()) {
+      setError("Please enter a message.");
+      return;
+    }
+    socket.emit("send_prompt", { prompt: inputMessage });
+    // Do not clear the inputMessage here if you want to retain the input until it's manually cleared
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { text: inputMessage, sender: "user" },
+    ]);
+    setInputMessage("");
+    setLoading(true);
+  };
+
+  return (
+    <Box bg="#3E4B5C" color="#E8F2FC" h="100vh" position="relative" py="2%">
+      <VStack spacing={4} align="stretch" px="10%">
+        {messages.length === 0 && (
+          <Flex
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+            mt="15%"
+          >
+            <Box
+              display="inline-block"
+              width="65px"
+              height="65px"
+              borderRadius="50%"
+              backgroundImage={logo}
+              backgroundSize="cover"
+            />
+
+            <Text fontSize="xl" fontWeight="bold" mt="2%">
+              How can I help you with your research today?
+            </Text>
+          </Flex>
+        )}
+        {messages.map((msg, index) => (
+          <Box
+            key={index}
+            bg={msg.sender === "user" ? "blue.500" : "gray.600"}
+            p={3}
+            borderRadius="md"
+          >
+            <Text fontSize="sm">
+              <b>{msg.sender === "user" ? "You" : "Lockness"}:</b> {msg.text}
+            </Text>
+          </Box>
+        ))}
+        {error && (
+          <Text color="red.500" mb={4}>
+            {error}
+          </Text>
+        )}
+        {loading && (
+          <VStack justifyContent="center" py="5%">
+            <Spinner color="white" />
+            <Text>Hmmm...</Text>
+          </VStack>
+        )}
+      </VStack>
+
+      <Flex
+        position="absolute"
+        bottom="2%"
+        justifyContent="center"
+        width="50%"
+        left="25%"
+      >
+        <VStack spacing={5} width="100%">
+
+        <InputGroup size="md">
+          <Textarea
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
+            placeholder="Enter new research prompt here..."
+            style={{
+              background: "#3E4B5C",
+              color: "#E8F2FC",
+              borderRadius: "8px",
+              width: "100%",
+              height: "15vh",
+              resize: "none", // Allows vertical resizing
+              overflowY: "auto", // Adds scroll if content overflows
+              borderColor: "#E8F2FC",
+              placeholderColor: "#B4B4B4",
+              padding: "2%",
+              fontSize: "sm",
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                handleSendMessage();
+                e.preventDefault();
+              }
+            }}
+          />
+
+          <Button
+            size="lg"
             color="#E8F2FC"
-            h="100vh"
-            position="relative"
-            py="2%"
-        >
-            <VStack spacing={4} align="stretch" px="10%">
-                {messages.map((msg, index) => (
-                    <Box
-                        key={index}
-                        bg={msg.sender === "user" ? "blue.500" : "gray.600"}
-                        p={3}
-                        borderRadius="md"
-                    >
-                        <Text fontSize="sm">
-                            <b>{msg.sender === "user" ? "You" : "Lockness"}:</b> {msg.text}
-                        </Text>
-                    </Box>
-                ))}
-                {error && (
-                    <Text color="red.500" mb={4}>
-                        {error}
-                    </Text>
-                )}
-                {loading && (<VStack justifyContent="center" py="5%"><Spinner color="white" /><Text>Thinking...</Text></VStack>)}
-            </VStack>
-            <HStack
-                mt={4}
-                position="absolute"
-                bottom="10"
-                left="40"
-                right="40"
-                spacing="5"
-            >
-                <Textarea
-                    value={inputMessage}
-                    onChange={(e) => setInputMessage(e.target.value)}
-                    placeholder="Enter your message..."
-                    colorScheme="white"
-                    autoFocus={true}
-                    size="md"
-                    color="white"
-                    outlineColor="white"
-                    borderRadius="md"
-                    onKeyDown={(e) => {
-                        if (e.key === "Enter" && !e.shiftKey) {
-                            handleSendMessage();
-                            e.preventDefault();
-                        }
-                    }}
-                />
-                <Button
-                    colorScheme="white"
-                    type="submit"
-                    borderRadius="md"
-                    outlineColor="white"
-                    onClick={handleSendMessage}
-                >
-                    <ArrowUpIcon />
-                </Button>
-            </HStack>
-        </Box >
-    );
+            onClick={handleSendMessage}
+            ml="1rem" // Adjust the margin to align the button as in the design
+            borderRadius="lg"
+            height="50%"
+            isDisabled={inputMessage === ""}
+          >
+            <ArrowUpIcon color="black" />
+          </Button>
+        </InputGroup>
+        <Text color="#B4B4B4" fontSize="sm">
+        Lockness AI produces results based on the query and the data it is fed. 
+    </Text>
+    </VStack>
+
+
+      </Flex>
+    </Box>
+  );
 };
 
 export default ChatInterface;
