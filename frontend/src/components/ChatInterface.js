e31cfb2f9ae14bcb4ffecc93279dd4946e004ecb
yuvansundrani <yuvan.sundrani@gmail.com>
2024-04-20 02:00:17 -0400

    bug fix

--- frontend/src/components/ChatInterface.js
+++ frontend/src/components/ChatInterface.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
 import {
-    Box,
-    VStack,
-    HStack,
-    Button,
-    Text,
-    Textarea,
-    Spinner,
-    Flex,
-    Center,
-    InputGroup,
-    InputRightElement,
-    useColorModeValue,
-    useToast,
-    Icon
+  Box,
+  VStack,
+  HStack,
+  Button,
+  Text,
+  Textarea,
+  Spinner,
+  Flex,
+  Center,
+  InputGroup,
+  InputRightElement,
+  useColorModeValue,
+  useToast,
+  Icon,
 } from "@chakra-ui/react";
 import io from "socket.io-client";
 import { ArrowUpIcon } from "@chakra-ui/icons";
@@ -28,8 +28,8 @@ import EnglishOutline from "./EnglishOutline";
 const domain = window.location.hostname;
 
 const socket = domain.includes("localhost")
-    ? io("http://localhost:5001")
-    : io("https://lockness-420607.uc.r.appspot.com");
+  ? io("http://localhost:5001")
+  : io("https://lockness-420607.uc.r.appspot.com");
 
 const ChatInterface = () => {
   const [messages, setMessages] = useState([
@@ -41,102 +41,103 @@ const ChatInterface = () => {
   const [inputMessage, setInputMessage] = useState("");
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [files, setFiles] = useState([]);
+  const fileInputRef = useRef(null);
+
+  const toast = useToast();
 
   const [step, setStep] = useState(-1);
 
-    useEffect(() => {
-        let currentMessage = { text: "", sender: "bot" };
+  useEffect(() => {
+    let currentMessage = { text: "", sender: "bot" };
 
-        socket.on("new_message", (message) => {
-            setLoading(false);
-            setError(null);
-            if (!message.final && message.text) {
-                currentMessage.text += message.text;
-                setMessages((prevMessages) => {
-                    // Update the last bot message if it exists, otherwise add a new one
-                    if (
-                        prevMessages.length &&
-                        prevMessages[prevMessages.length - 1].sender === "bot"
-                    ) {
-                        return [...prevMessages.slice(0, -1), currentMessage];
-                    } else {
-                        return [...prevMessages, currentMessage];
-                    }
-                });
-            } else if (message.final && currentMessage.text) {
-                // Push the complete message only if there's text in the current message
-                // setMessages(prevMessages => [...prevMessages, currentMessage]);
-                currentMessage = { text: "", sender: "bot" }; // Reset for the next message
-            }
+    socket.on("new_message", (message) => {
+      setLoading(false);
+      setError(null);
+      if (!message.final && message.text) {
+        currentMessage.text += message.text;
+        setMessages((prevMessages) => {
+          // Update the last bot message if it exists, otherwise add a new one
+          if (
+            prevMessages.length &&
+            prevMessages[prevMessages.length - 1].sender === "bot"
+          ) {
+            return [...prevMessages.slice(0, -1), currentMessage];
+          } else {
+            return [...prevMessages, currentMessage];
+          }
         });
+      } else if (message.final && currentMessage.text) {
+        // Push the complete message only if there's text in the current message
+        // setMessages(prevMessages => [...prevMessages, currentMessage]);
+        currentMessage = { text: "", sender: "bot" }; // Reset for the next message
+      }
+    });
 
-        socket.on("connect_error", (err) => {
-            setError("WebSocket connection failed.");
-            setLoading(false);
+    socket.on("connect_error", (err) => {
+      setError("WebSocket connection failed.");
+      setLoading(false);
+    });
+
+    return () => {
+      socket.off("new_message");
+      socket.off("connect_error");
+    };
+  }, []);
+
+  const handleSendMessage = () => {
+    if (!inputMessage.trim()) {
+      setError("Please enter a message.");
+      return;
+    }
+    socket.emit("send_prompt", { prompt: inputMessage });
+    // Do not clear the inputMessage here if you want to retain the input until it's manually cleared
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { text: inputMessage, sender: "user" },
+    ]);
+    setInputMessage("");
+    setLoading(true);
+  };
+
+  const handleUploadFileClick = () => {
+    fileInputRef.current.click();
+  };
+
+  // add file to the staging area - file will be uploaded with the message
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      // check file type, ensure it is a csv file, txt file, or pdf
+      if (
+        file.type !== "text/csv" &&
+        file.type !== "text/plain" &&
+        file.type !== "application/pdf"
+      ) {
+        toast({
+          title: "Invalid file type.",
+          description: "Please upload a .csv, .txt, or .pdf file.",
+          status: "error",
+          duration: 1500,
+          isClosable: true,
         });
+        return;
+      }
 
-        return () => {
-            socket.off("new_message");
-            socket.off("connect_error");
-        };
-    }, []);
+      // add file to the staging area, by appending to the files array
+      setFiles([...files, file]);
+      // clear the file input
+      event.target.value = null;
 
-    const handleSendMessage = () => {
-        if (!inputMessage.trim()) {
-            setError("Please enter a message.");
-            return;
-        }
-        socket.emit("send_prompt", { prompt: inputMessage });
-        // Do not clear the inputMessage here if you want to retain the input until it's manually cleared
-        setMessages((prevMessages) => [
-            ...prevMessages,
-            { text: inputMessage, sender: "user" },
-        ]);
-        setInputMessage("");
-        setLoading(true);
-    };
-
-    const handleUploadFileClick = () => {
-        fileInputRef.current.click();
-    };
-
-    // add file to the staging area - file will be uploaded with the message
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            // check file type, ensure it is a csv file, txt file, or pdf
-            if (
-                file.type !== "text/csv" &&
-                file.type !== "text/plain" &&
-                file.type !== "application/pdf"
-            ) {
-                toast({
-                    title: "Invalid file type.",
-                    description: "Please upload a .csv, .txt, or .pdf file.",
-                    status: "error",
-                    duration: 1500,
-                    isClosable: true,
-                });
-                return;
-            }
-
-            // add file to the staging area, by appending to the files array
-            setFiles([...files, file]);
-            // clear the file input
-            event.target.value = null;
-
-            toast({
-                title: "File uploaded successfully.",
-                description: file.name,
-                status: "success",
-                duration: 1500,
-                isClosable: true,
-            });
-        }
-    };
-
-
-
+      toast({
+        title: "File uploaded successfully.",
+        description: file.name,
+        status: "success",
+        duration: 1500,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
     <Box
@@ -169,47 +170,44 @@ const ChatInterface = () => {
               backgroundSize="cover"
             />
 
-                        <Text fontSize="xl" fontWeight="bold" mt="2%">
-                            How can I help you with your research today?
-                        </Text>
-                    </Flex>
-                )}
-                {messages.map((msg, index) => (
-                    <Center>
-                        <Box
-                            width="100%"
-                            key={index}
-                            // bg={msg.sender === "user" ? "blue.500" : "gray.600"}
-                            p={5}
-                            borderRadius="md"
-                        >
-                            <Flex alignItems="center" justifyContent="start">
-                                <Box
-                                    display="inline-block"
-                                    width="40px"
-                                    height="40px"
-                                    borderRadius="50%"
-                                    backgroundImage={
-                                        msg.sender === "user" ? defaultProfilePicture : logo
-                                    }
-                                    backgroundSize="cover"
-                                />
-                                <Text ml="1%" size="lg" fontWeight="bold">
-                                    {msg.sender === "user" ? "You" : "Lockness AI"}
-                                </Text>
-                            </Flex>
+            <Text fontSize="xl" fontWeight="bold" mt="2%">
+              How can I help you with your research today?
+            </Text>
+          </Flex>
+        )}
+        {messages.map((msg, index) => (
+          <Center>
+            <Box
+              width="100%"
+              key={index}
+              // bg={msg.sender === "user" ? "blue.500" : "gray.600"}
+              p={5}
+              borderRadius="md"
+            >
+              <Flex alignItems="center" justifyContent="start">
+                <Box
+                  display="inline-block"
+                  width="40px"
+                  height="40px"
+                  borderRadius="50%"
+                  backgroundImage={
+                    msg.sender === "user" ? defaultProfilePicture : logo
+                  }
+                  backgroundSize="cover"
+                />
+                <Text ml="1%" size="lg" fontWeight="bold">
+                  {msg.sender === "user" ? "You" : "Lockness AI"}
+                </Text>
+              </Flex>
 
               {msg.sender === "user" ? (
                 <Text fontSize="md" mt="1%" ml="5%">
                   {msg.text}
                 </Text>
+              ) : step === 0 ? (
+                <EnglishOutline outlineContent={msg.text} />
               ) : (
-                step === 0 ? 
-                <EnglishOutline outlineContent={msg.text}/> :
                 <Text>More than step 0 </Text>
-
-
-                
               )}
             </Box>
           </Center>
@@ -237,6 +235,28 @@ const ChatInterface = () => {
         <VStack spacing={5} width="100%">
           {step === -1 && (
             <InputGroup size="md">
+              <Flex
+                alignItems="center"
+                justifyContent="left"
+                borderRadius="lg"
+                p="6.5%"
+                cursor="pointer"
+                // _hover={{ bg: "#3E4B5C" }}
+                onClick={handleUploadFileClick}
+              >
+                <Icon
+                  aria-label="Upload File"
+                  as={AttachmentIcon}
+                  bg="transparent"
+                  colorScheme="white"
+                ></Icon>
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  onChange={handleFileChange}
+                  style={{ display: "none" }}
+                />
+              </Flex>
               <Textarea
                 value={inputMessage}
                 onChange={(e) => setInputMessage(e.target.value)}
