313680269494f9263a35f7e4978efe7890626a1c
yuvansundrani <yuvan.sundrani@gmail.com>
2024-04-20 21:07:01 -0400

    Ui cleanup, using rest a lot more

---
+++ frontend/src/components/MarkdownButton.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Text } from "@chakra-ui/react";
+
+const MarkdownButton = ({ onClick, ...rest }) => {
+  return (
+    <Button
+      ml={rest.ml}
+      onClick={() => {
+        console.log("Turning on editing for this outline");
+        onClick(); // Make sure to call the onClick prop if it's provided
+      }}
+      width={rest.width}
+      backgroundColor={rest.backgroundColor}
+      _active={{
+        backgroundColor: rest.backgroundColor,
+      }}
+      _focus={{
+        boxShadow: "none",
+        backgroundColor: rest.backgroundColor,
+      }}
+      _hover={{
+        transform: "scale(1.05)",
+      }}
+    >
+      <Text color={rest.textColor}>{rest.buttonText}</Text>
+    </Button>
+  );
+};
+
+export default MarkdownButton;
