417146c03ef94bfb6e61a8c2d0f9d70831d54dae
Jason Steinberg <j.steinberg702@gmail.com>
2024-04-24 18:03:31 -0400

    updated tests

--- backend/tests/test_linter.py
+++ backend/tests/test_linter.py
@@ -1,5 +1,6 @@
+import os
 import sys
-sys.path.append('..')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 from services.linting_service import LintingService
 
--- backend/tests/test_query_mocker.py
+++ backend/tests/test_query_mocker.py
@@ -1,9 +1,20 @@
+import os
 import sys
-sys.path.append('..')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 from services.query_mocker import QueryMocker
 
+def test_query_mocker_convert_column_data_to_dict():
+    test_cols_file = open("assets/test_column_data.txt", "r")
+    test_cols_str = test_cols_file.read()
+    test_cols_file.close()
+    
+    test_cols_dict = QueryMocker.convert_column_data_to_dict(test_cols_str)
 
+    print(test_cols_dict)
+
+    assert "Master_Beneficiary_Summary_File_MBSF_Base" in test_cols_dict
+    
 def test_query_mocker_simple():
     query = "SELECT * FROM mytable;"
     columns = ["id", "name", "age"]
@@ -13,9 +24,9 @@ def test_query_mocker_simple():
 
 def test_query_mocker_realistic():
     # load string from txt file to use columns
-    test_cols_str = open("test_columns.txt", "r")
-    test_cols = test_cols_str.read().splitlines()
-    test_cols_str.close()
+    test_cols_file = open("assets/test_column_data.txt", "r")
+    test_cols_str = test_cols_file.read()
+    test_cols_file.close()
 
     query = """
 SELECT COUNT(*) AS beneficiaries_with_lung_cancer
@@ -26,6 +37,8 @@ WHERE mbsf_base.STATE_CD = 'KY'
 AND mbsf_chronic.CANCER_LUNG_EVER IS NOT NULL
 AND mbsf_chronic.BENE_ENROLLMT_REF_YR = 2021;
 """
-    mocker = QueryMocker(query, test_cols)
+
+    test_cols_dict = QueryMocker.convert_column_data_to_dict(test_cols_str)
+    mocker = QueryMocker(query, test_cols_dict)
     result = mocker.run_mock_query()
     assert result == True
