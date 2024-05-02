3b5d07ba237503ffc5b2d176dcee849a0bdef8a4
Jason Steinberg <j.steinberg702@gmail.com>
2024-05-01 22:24:31 -0400

    improved linter

--- backend/services/linting_service.py
+++ backend/services/linting_service.py
@@ -15,21 +15,27 @@ class LintingService:
         # Lint the SQL string
         try:
             linting_result = linter.lint_string(sql_string)
+            violations =  linting_result.get_violations()
+            
         except SQLParseError as e:
             # add the error to the linting result
-            errors = [str(e.message)]
+            errors.append(str(e))
         
+        # FOR NOW, ONLY RETURN THE ERRORS (ignore the violations)
         # Filter out the linting errors and warnings to only include ones we care about
         for violation in linting_result.get_violations():
             # Filter out errors/warnings we don't care about
-            if not violation.description in [
-                "Query produces an unknown number of result columns.",
-                "Unnecessary trailing whitespace at end of file.",
-                "Files must end with a trailing newline.",
-                "Files must not begin with newlines or whitespace.",
-                "Unnecessary trailing whitespace.",
-            ]:
-                filtered_violations.append(violation)
+            # if not violation.description in [
+            #     "Query produces an unknown number of result columns.",
+            #     "Unnecessary trailing whitespace at end of file.",
+            #     "Files must end with a trailing newline.",
+            #     "Files must not begin with newlines or whitespace.",
+            #     "Unnecessary trailing whitespace.",
+            # ]:
+            #     filtered_violations.append(violation)
+            if type(violation).__name__ == "SQLParseError":
+                errors.append(str(violation.description))
+                
 
         # Return the number of linting errors and warnings
         return filtered_violations, errors
--- backend/tests/test_linter.py
+++ backend/tests/test_linter.py
@@ -10,8 +10,9 @@ SELECT *
 FROM mytable
 WHERE id = 1;
     """
-    violations = LintingService.lint_sql(sql)
+    violations, errors = LintingService.lint_sql(sql)
     assert len(violations) == 0
+    assert len(errors) == 0
 
 def test_lint_sql_valid2():
     sql = """
@@ -21,8 +22,9 @@ WHERE
     id = 1
     AND name = 'John';
     """
-    violations = LintingService.lint_sql(sql)
+    violations, errors = LintingService.lint_sql(sql)
     assert len(violations) == 0
+    assert len(errors) == 0
 
 def test_lint_sql_valid3():
     sql = """
@@ -32,23 +34,49 @@ JOIN Inpatient_FeeForService USING (BENE_ID)
 JOIN Outpatient_FeeForService USING (BENE_ID)
 JOIN Carrier_FeeForService USING (BENE_ID)
 WHERE STATE_CD = 'KS'
-AND EXTRACT(YEAR FROM DEATH_DT) = 2018
-AND -- Add additional conditions here to identify car accident-related deaths, e.g., diagnosis codes.
-;
+AND EXTRACT(YEAR FROM DEATH_DT) = 2018;
     """
-    violations = LintingService.lint_sql(sql)
+    violations, errors = LintingService.lint_sql(sql)
     assert len(violations) == 0
+    assert len(errors) == 0
 
-def test_lint_sql_invalid():
+def test_lint_sql_invalid_select():
     sql = """
 SELECT *
 regewrg mytable;
-    """
-    violations = LintingService.lint_sql(sql)
-    assert len(violations) == 1
+"""
+    violations, errors = LintingService.lint_sql(sql)
+    assert len(errors) >= 1
+    assert "Found unparsable section:" in errors[0]
 
-    print(violations)
-    assert "Found unparsable section:" in violations[0].description
+
+def test_lint_sql_invalid_where():
+    sql = """
+SELECT *
+FROM mytable
+WHERE
+AND column = value;
+"""
+    violations, errors = LintingService.lint_sql(sql)
+    assert len(errors) >= 1
+    assert "Found unparsable section:" in errors[0]
+
+
+def test_lint_sql_invalid_order_by():
+    sql = """
+SELECT *
+FROM mytable
+ORDER BY;
+"""
+    violations, errors = LintingService.lint_sql(sql)
+    assert len(errors) >= 1
+    assert "Found unparsable section:" in errors[0]
+
+
+
+
+
+
 
     # Check that the expected violation is reported
     # expected_violation = "syntax error"
